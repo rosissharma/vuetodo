2a8f86d7926ccdc99b73b43f4fec6d1a4ca9d9fa
rs9110 <46945026+rs9110@users.noreply.github.com>
2020-10-21 15:18:19 -0400

    Update Todos.vue

--- components/Todos.vue
+++ components/Todos.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="container">
       <div v-for="todo in todos" v-bind:key="todo.id">
-        <ul class="list-group" v-bind:class="{ completed: todo.completed }">
+        <ul class="list-group"">
           <li class="list-group-item">
-            <input type="checkbox" v-bind:checked="todo.completed" @change="completed" />
             {{ todo.task }}
             <button @click="$emit('delete-todo', todo.id)" class="btn btn-danger">X</button>
           </li>
@@ -17,22 +16,12 @@
 export default {
   name: "Todos",
   props: ["todos"],
-  methods: {
-      completed() {
-          this.todo.completed = !this.todo.completed
-      }
-  }
 };
 </script>
 
 <style>
 .list-group-item {
   margin: 5px;
-}
-
-.completed {
-  text-decoration: line-through;
-  color: #aaa;
 }
 
 .btn {
